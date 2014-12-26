ba2ece25dd5222e39fdfe2fae1ab4ac3c2c795b2
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-12-26 23:48:24 +0100

    Added some extra checks to avoid builds errors and only to run when directories are present and fixed CPU usage message oopsies

--- lilo.go
+++ lilo.go
@@ -3,6 +3,8 @@ package main
 import (
 	"os"
 	"runtime"
+	"strconv"
+	"strings"
 
 	stitcher "github.com/WyriMaps/MinimapStitcher"
 	"github.com/codegangsta/cli"
@@ -13,8 +15,18 @@ func main() {
 	app.Name = "sitcher"
 	app.Usage = "Sitcher World of Warcraft minimaps together"
 	app.Action = func(c *cli.Context) {
+		_, errTiles := os.Stat("/opt/map_tiles/")
+		if os.IsNotExist(errTiles) {
+			return
+		}
+
+		_, errMaps := os.Stat("/opt/maps/")
+		if os.IsNotExist(errMaps) {
+			return
+		}
+
 		runtime.GOMAXPROCS(runtime.GOMAXPROCS(runtime.NumCPU())) // Auto detects the number of cores to use
-		s := []string{"Found", runtime.NumCPU(), "CPU cores we'll be using"};
+		s := []string{"Found", strconv.Itoa(runtime.NumCPU()), "CPU cores we'll be using"};
 		println(strings.Join(s, " "));
 		var report = func(message map[string]string) {
 			println(message["minimap"] + ": " + message["type"]);
